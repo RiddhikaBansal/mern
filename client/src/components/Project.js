5fa77f253478d4042f4e5a529ec68da5c7d2bdda
RiddhikaBansal <123889146+RiddhikaBansal@users.noreply.github.com>
2024-11-02 00:48:58 +0530

    Update Project.js

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,27 +1,25 @@
-// src/components/Project.js
 import React from 'react';
 
 const Project = ({ project }) => {
-    return (
-        <div className="project">
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
+  return (
+    <div className="project">
+      <h3>{project.title}</h3>
+      <p>{project.description}</p>
 
-            {/* Display multiple images if they exist */}
-            {project.images ? (
-                <div className="project-images">
-                    {project.images.map((img, index) => (
-                        <img key={index} src={img} alt={` ${project.title}`} className="project-image" />
-                    ))}
-                </div>
-            ) : (
-                <img src={project.image}  className="project-image" />
-            )}
-
-            
-            <a href={project.github} target="_blank" rel="noopener noreferrer">View code on GitHub</a>
+      {/* Display multiple images if they exist */}
+      {project.images && project.images.length > 0 ? (
+        <div className="project-images">
+          {project.images.map((img, index) => (
+            <img key={index} src={img} alt={`${project.title}`} className="project-image" />
+          ))}
         </div>
-    );
+      ) : (
+        <img src={project.image}  className="project-image" />
+      )}
+
+      <a href={project.github} target="_blank" rel="noopener noreferrer">View code on GitHub</a>
+    </div>
+  );
 };
 
 export default Project;
